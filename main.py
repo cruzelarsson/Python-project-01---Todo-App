c35353ad2e75f07e1352a79c177ee016508ea3f6
Larsson <larssoncruze@gmail.com>
2025-10-09 03:06:15 -0400

    Added various improvements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, exit or mark completed task: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -11,15 +11,19 @@ while True:
             todos.append(todo)
 
         case 'show':
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index , item in enumerate(todos):
+                row = f"{index+1}.{item}"
+                print(row)
 
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             new_todo = input ("Enter the new todo: ")
             todos[number-1] = new_todo
             print(todos[number-1])
+
+        case 'complete':
+            number = int(input("Number of completed tasks: "))
+            todos.pop(number-1)
 
         case 'exit':
             break
